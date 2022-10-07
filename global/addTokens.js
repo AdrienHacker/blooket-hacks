30ea4aa393a025b658f615b018a3c0b29bd249d1
AdrienHacker <115197019+AdrienHacker@users.noreply.github.com>
2022-10-07 13:02:39 -0400

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -50,8 +50,8 @@
                         },
                         body: await encodeValues({
                             name: x.name,
-                            addedTokens: 500,
-                            addedXp: 300
+                            addedTokens: 0,
+                            addedXp: 0
                         }, e.secret)
                     }).then(() => alert('Added daily rewards!')).catch(() => alert('There was an error when adding rewards!'));;
                 }).catch(() => alert('There was an error encoding requests!'));
